0de0bf22d8711799682341abc54d65d9d5927e43
EfeDeveloper <edwinvilla_adsi@outlook.com>
2022-08-28 16:55:31 -0500

    ✨ feat(deploy):
    Agregando proyecto curso Software Debugging

---
+++ items.js
@@ -0,0 +1,47 @@
+var items = [
+  {
+    imageUrl: 'https://images.pexels.com/photos/1448055/pexels-photo-1448055.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'adult Labrador retriever',
+    title: 'Marcus',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/803766/pexels-photo-803766.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'ice caves in the wild landscape photo of ice near ...',
+    title: 'Margarita',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/7524674/pexels-photo-7524674.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: '@adventure.yuki frozen grass short-coated black do...',
+    title: 'Kayla',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/540524/pexels-photo-540524.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'Hiking with my dog in the woods. white labrador re...',
+    title: 'Sibylle',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/6530675/pexels-photo-6530675.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'We are a couple with two beautiful dog children...',
+    title: 'Jolanda',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/5256186/pexels-photo-5256186.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'Bone salt and pepper schnauzer puppy',
+    title: 'Pwry',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/604533/pexels-photo-604533.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'Dog in a forest at sunset dog in forest with sun r...',
+    title: 'Vanessa',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/3196887/pexels-photo-3196887.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'black and white Husky',
+    title: 'Pwry',
+  },
+  {
+    imageUrl: 'https://images.pexels.com/photos/1033116/pexels-photo-1033116.jpeg?auto=compress&cs=tinysrgb&w=500',
+    subtitle: 'Milo durmiendo después de un largo día de jugar en...',
+    title: 'Rudi',
+  },
+];
