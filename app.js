c7d59e9ee4e96f583fcfca144b3b1e02cbbfe281
EfeDeveloper <edwinvilla_adsi@outlook.com>
2022-08-28 19:09:19 -0500

    🐛 fix(add):
    agregando process.env.PORT en el app.listen de la api

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 
 var quotes = [
